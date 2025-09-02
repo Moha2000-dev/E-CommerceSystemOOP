572789bdbc3e2db37441aecc49a0cfb540fc4c44
MOHAMMED <mohaalialalawi20@gmail.com>
2025-09-02 14:59:59 +0400

    add the supplier repo

---
+++ Repositories/ISupplierRepository.cs
@@ -0,0 +1,11 @@
+﻿using E_CommerceSystem.Models;
+namespace E_CommerceSystem.Repositories;
+public interface ISupplierRepository
+{
+    Task<Supplier?> GetAsync(int id);
+    Task<List<Supplier>> GetAllAsync();
+    Task AddAsync(Supplier entity);
+    void Update(Supplier entity);
+    void Delete(Supplier entity);
+    Task<int> SaveAsync();
+}
---
+++ Repositories/SupplierRepository.cs
@@ -0,0 +1,16 @@
+﻿using E_CommerceSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceSystem.Repositories;
+public class SupplierRepository : ISupplierRepository
+{
+    private readonly ApplicationDbContext _db;
+    public SupplierRepository(ApplicationDbContext db) => _db = db;
+
+    public Task<Supplier?> GetAsync(int id) => _db.Suppliers.FindAsync(id).AsTask();
+    public Task<List<Supplier>> GetAllAsync() => _db.Suppliers.AsNoTracking().ToListAsync();
+    public Task AddAsync(Supplier e) => _db.Suppliers.AddAsync(e).AsTask();
+    public void Update(Supplier e) => _db.Suppliers.Update(e);
+    public void Delete(Supplier e) => _db.Suppliers.Remove(e);
+    public Task<int> SaveAsync() => _db.SaveChangesAsync();
+}
