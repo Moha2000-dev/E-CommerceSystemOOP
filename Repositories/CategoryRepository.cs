a0b442e9a66b9ebbee563156cbbdded990aa10d2
MOHAMMED <mohaalialalawi20@gmail.com>
2025-09-02 14:56:55 +0400

    add the catogryrepositer

---
+++ Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using E_CommerceSystem.Models;
+using E_CommerceSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceSystem.Infrastructure.Repositories;
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _db;
+    public CategoryRepository(ApplicationDbContext db) => _db = db;
+
+    public Task<Category?> GetAsync(int id) => _db.Categories.FindAsync(id).AsTask();
+    public Task<List<Category>> GetAllAsync() => _db.Categories.AsNoTracking().ToListAsync();
+    public Task AddAsync(Category e) => _db.Categories.AddAsync(e).AsTask();
+    public void Update(Category e) => _db.Categories.Update(e);
+    public void Delete(Category e) => _db.Categories.Remove(e);
+    public Task<int> SaveAsync() => _db.SaveChangesAsync();
+}
---
+++ Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using E_CommerceSystem.Models;
+
+namespace E_CommerceSystem.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Category?> GetAsync(int id);
+        Task<List<Category>> GetAllAsync();
+        Task AddAsync(Category entity);
+        void Update(Category entity);
+        void Delete(Category entity);
+        Task<int> SaveAsync();
+    }
+}
