6bd85d926f01c9522bd8ee8b21c7abc97f953177
MOHAMMED <mohaalialalawi20@gmail.com>
2025-09-02 19:37:59 +0400

    FIX MIGRATIONS ERRORES

---
+++ ApplicationDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿// ApplicationDbContextFactory.cs
+using E_CommerceSystem;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Load configuration (works for both design time and run time)
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var cs = config.GetConnectionString("DefaultConnection");
+
+        var opts = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlServer(cs)
+            .Options;
+
+        return new ApplicationDbContext(opts);
+    }
+}
---
+++ Migrations/20250902153229_URLCOMESWORKS1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_CommerceSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class URLCOMESWORKS1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Products_PID",
+                table: "OrderProducts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Products_PID",
+                table: "OrderProducts",
+                column: "PID",
+                principalTable: "Products",
+                principalColumn: "PID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Products_PID",
+                table: "OrderProducts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Products_PID",
+                table: "OrderProducts",
+                column: "PID",
+                principalTable: "Products",
+                principalColumn: "PID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
